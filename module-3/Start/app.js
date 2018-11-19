fcf22d423e3fd6f8ff85b45721c42276074e490c
Josh Cole <colejc@mail.missouri.edu>
2018-11-19 14:55:27 -0600

    Create new routes api and authenticate

--- module-3/Start/app.js
+++ module-3/Start/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var api = require('./routes/api');
+//var authenticate = require('./routes/authenticate');
 
 var app = express();
 
